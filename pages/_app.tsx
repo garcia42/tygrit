57e6ec4ec45651c5833a181c301701e3d061eb6c
Jesus Garcia <jesusvgarcia42@gmail.com>
2022-02-27 02:26:00 -0800

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,46 @@
 import type { AppProps } from 'next/app'
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  const handleRouteChange = (url:string) => {
+    (window as any).gtag('config', process.env.NEXT_PUBLIC_GA, {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+  <>
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
+    />
+    <Script
+      id="gtag-init"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA}', {
+            page_path: window.location.pathname,
+          });
+        `,
+      }}
+    />
+    <Component {...pageProps} />
+  </>);
 }
 
 export default MyApp
