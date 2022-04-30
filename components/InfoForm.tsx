f59ceb6056976e9ad8605b348e54aa6450f04cb9
Jesus Garcia <jesusvgarcia42@gmail.com>
2022-04-30 14:51:47 -0700

    Finish UI and email

---
+++ components/InfoForm.tsx
@@ -0,0 +1,140 @@
+import { useState } from "react";
+
+interface InfoFormPropTypes {
+    setSuccessToast: (s: string) => void,
+    setFailToast: (s: string) => void,
+}
+
+export const API_URL = process.env.NEXT_PUBLIC_VERCEL_URL ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL : "http://localhost:3000";
+
+function recordEmail(email: string, name: string, address: string, phone: string) {
+  return fetch('https://api.sendinblue.com/v3/contacts', {
+    method: 'POST',
+    headers: {
+      'api-key': process.env.NEXT_PUBLIC_SEND_BLUE as string,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email,
+      FNAME: name,
+      LNAME: address,
+      SMS: phone,
+      "updateEnabled": false,
+      "listIds": [
+        2,
+      ],
+    }),
+  })
+}
+
+function sendConfirmationEmail(email: string) {
+  return fetch('https://api.sendinblue.com/v3/smtp/email', {
+    method: 'POST',
+    headers: {
+      'api-key': process.env.NEXT_PUBLIC_SEND_BLUE as string,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      sender: {
+        email: "jesus.garcia@tygritinvesting.com",
+        name: "Jesus at Tygrit Investing"
+      },
+      to: [
+        {
+          email,
+        }
+      ],
+      replyTo: {
+        email: "jesus.garcia@tygritinvesting.com"
+      },
+      templateId: 2,
+    })
+  });
+}
+
+export default function InfoForm({setSuccessToast, setFailToast}: InfoFormPropTypes) {
+    let [email, setEmail] = useState<string>('');
+    let [name, setName] = useState<string>('');
+    let [phone, setPhone] = useState<string>('');
+    let [address, setAddress] = useState<string>('');
+    let [isInPark, setInPark] = useState<boolean>(false);
+  
+    const onChangeRadio = (event: any) => {
+      console.log(event.target.value);
+      event.target.value === "private" ? setInPark(false) : setInPark(true)
+    }
+
+    async function submitEmail(e: React.SyntheticEvent, email: string) {
+        e.preventDefault();
+
+        if (!email) {
+            setFailToast("Please enter an email");
+            return;
+        }
+        
+        return await recordEmail(email, name, address, phone)
+            .then((resp) => resp.json()).then(data => {
+                if (!data.code) {
+                    return sendConfirmationEmail(email)
+                } else {
+                    setFailToast(data.message);
+                    throw new Error('Something went wrong confirming email');
+                }
+            }, (e) => {
+                console.log(e);
+            })
+            .then((resp: any) => (resp as Response).json()).then((data: any) => {
+                if (!data.code) {
+                    setSuccessToast("Success");
+                    (window as any).gtag('event', 'conversion', {"send_to": process.env.NEXT_PUBLIC_GOOGLE_ADS_CONV});
+                    (window as any).gtag('event', 'sign_up', {"send_to": process.env.NEXT_PUBLIC_GA});
+                } else {
+                    setFailToast(data.message);
+                    throw new Error('Something went wrong sending email');
+                }
+            }, (e: any) => {
+                console.log(e);
+            });
+        }
+  
+    return (
+      <div className="m-4 bg-gray-200 p-4">
+        <p className="text-xl font-semibold">Mobile Home Cash Offer</p>
+        <p className="">Fill out the information below to get started or call 916-912-8488</p>
+        <form className="mt-2" onSubmit={(e) => submitEmail(e, email)}>
+          <div className="my-1">
+            <p>Your name<span className="text-red-400">*</span></p>
+            <input required={true} className="border border-gray-500 p-2 flex w-full rounded-lg" type="address" name="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name"></input>
+          </div>
+          <div className="my-1">
+            <p>Property Address<span className="text-red-400">*</span></p>
+            <input required={true} className="border border-gray-500 p-2 flex w-full rounded-lg" type="address" name="email" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="Address"></input>
+          </div>
+          {/* <div className="my-1">
+            <p>My home is...<span className="text-red-400">*</span></p>
+            <ul className=" flex">
+              <li className=" inline text-center items-center relative flex-1">
+                <input type="radio" id="lot" className="mr-1 border border-gray-500 rounded-lg" value="private" name="email" checked={!isInPark} onChange={(e) => onChangeRadio(e)}></input> On a private lot
+              </li>
+              <li className=" inline relative flex-1">
+                <input type="radio" id="park" className="mr-1 border border-gray-500 rounded-lg" value="inPark" name="email" checked={!isInPark} onChange={(e) => onChangeRadio(e)}></input> In a park
+              </li>
+            </ul>
+          </div> */}
+          <div className="my-1">
+            <p>Phone<span className="text-red-400">*</span></p>
+            <input required={true} className="border border-gray-500 p-2 flex w-full rounded-lg" name="email" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="Phone number"></input>
+          </div>
+          <div className="my-1">
+            <p>Email<span className="text-red-400">*</span></p>
+            <input required={true} className="border border-gray-500 p-2 flex w-full rounded-lg" name="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email address"></input>
+          </div>
+          <div className="flex mt-2">
+            <button className="text-white bg-red-600 p-2 mx-auto w-6/12 rounded-lg">Get my Cash Offer</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,58 +5,11 @@ import Image from 'next/image'
 import Link from "next/link"
 import { useState } from "react"
 import Toasts from "../components/Toasts"
-import ReadySlider from "../components/ReadySlider"
-
-export const API_URL = process.env.NEXT_PUBLIC_VERCEL_URL ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL : "http://localhost:3000";
-
-function recordEmail(email: string) {
-  return fetch('https://api.sendinblue.com/v3/contacts', {
-    method: 'POST',
-    headers: {
-      'api-key': process.env.NEXT_PUBLIC_SEND_BLUE as string,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      email,
-      "updateEnabled": false,
-      "listIds": [
-        2,
-      ],
-    }),
-  })
-}
-
-function sendConfirmationEmail(email: string) {
-  return fetch('https://api.sendinblue.com/v3/smtp/email', {
-    method: 'POST',
-    headers: {
-      'api-key': process.env.NEXT_PUBLIC_SEND_BLUE as string,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      sender: {
-        email: "jesus.garcia@asyncchesscoach.com",
-        name: "AsyncChessCoach"
-      },
-      to: [
-        {
-          email,
-        }
-      ],
-      replyTo: {
-        email: "jesus.garcia@asyncchesscoach.com"
-      },
-      templateId: 2,
-    })
-  });
-}
+import InfoForm from "../components/InfoForm"
 
 const cardStyle = {
-  "marginTop": "1rem",
-  "padding": "1.5rem",
-  "textAlign": "left",
+  "padding": ".5rem",
+  "textAlign": "center",
   "color": "inherit",
   "textDecoration": "none",
   "border": "1px solid #eaeaea",
@@ -67,183 +20,155 @@ const cardStyle = {
 // typeof window !== 'undefined' && localStorage.setItem('seenReadySlider', '');
 
 const Home: NextPage = () => {
-
-  let [email, setEmail] = useState<string>('');
   let [successToast, setSuccessToast] = useState<string>("");
   let [failToast, setFailToast] = useState<string>("");
 
-  let [showSlider, setShowSlider] = useState<boolean>(typeof window !== 'undefined' && !localStorage.getItem('seenReadySlider'));
-  if (typeof window !== 'undefined' && showSlider) {
-    document.body.style.overflow = 'hidden';
-  }
-
-  async function submitEmail(e: React.SyntheticEvent, email: string) {
-    e.preventDefault();
-
-    if (!email) {
-      setFailToast("Please enter an email");
-      return;
-    }
-  
-    return await recordEmail(email)
-      .then((resp) => resp.json()).then(data => {
-        if (!data.code) {
-          return sendConfirmationEmail(email)
-        } else {
-          setFailToast(data.message);
-          throw new Error('Something went wrong confirming email');
-        }
-      }, (e) => {
-        console.log(e);
-      })
-      .then(resp => (resp as Response).json()).then(data => {
-        if (!data.code) {
-          setSuccessToast("Success");
-          (window as any).gtag('event', 'conversion', {"send_to": process.env.NEXT_PUBLIC_GOOGLE_ADS_CONV});
-          (window as any).gtag('event', 'sign_up', {"send_to": process.env.NEXT_PUBLIC_GA});
-        } else {
-          setFailToast(data.message);
-          throw new Error('Something went wrong sending email');
-        }
-      }, (e) => {
-        console.log(e);
-      });
-  }
-
   return (
-    <div className="mx-4 sm:mx-16 lg:mx-24">
+    <div className="">
       <Head>
         <title>Async Chess Coach</title>
         <meta name="description" content="Games reviewed in the background" />
         <link rel="icon" href="/chess-strategy.png" />
       </Head>
 
-      <div className="flex my-3">
+      <div className="flex py-4 bg-red-400 px-4 sm:px-16 lg:px-24 justify-center">
           <Link href={"/"}>
-            <a className="font-bold flex">
-              <Image width={30} height={30} alt="logo" src={"/chess-strategy.png"}/>
-              <div className="self-center">Async Chess Coach</div>
+            <a className="font-semibold flex">
+              <div className="self-center text-center text-white">SELL YOUR MOBILE HOME IN GEORGIA TODAY!</div>
             </a>
           </Link>
       </div>
 
-      <main className="justify-center align-center mt-14 md:mt-24">
+      <main className="justify-center align-center">
+        <div className="relative">
+          <div className="mx-auto sm:flex justify-center relative bg-[url('/mobileHome.jpeg')] bg-cover">
+            <div className="bg-cover bg-gradient-to-b from-gray-500 absolute top-0 w-full h-full opacity-50"/>
+            <div className="md:flex p-4 z-0 relative">
+              <div className="text-left self-center pb-4 md:pr-8 max-w-lg text-white">
 
-        <div className="mx-auto md:flex justify-center">
-          <div className="text-left self-center mb-4 md:mr-16 max-w-lg">
+                <h1 className="font-semibold text-2xl md:text-3xl md:pt-0 pt-4 text-white">
+                  We Buy Mobile Homes and Land in Augusta, Georgia
+                </h1>
 
-            <h1 className="font-semibold text-4xl md:text-5xl my-2">
-              Get your chess games analyzed by masters
-            </h1>
-
-            <h5 className="text-lg md:text-xl">
-              Tailored feedback for your games
-            </h5>
-
-            <div className="bg-opacity-50 mt-4 text-lg md:text-xl">
-              <p>Sign up to be notified of the beta release</p>
-              <form className=" bg-opacity-50 bg-black inline-flex" onSubmit={(e) => submitEmail(e, email)}>
-                <input name="email" value={email} onChange={(e) => setEmail(e.target.value)} className="flex-4 border border-gray-500 pl-1" placeholder="email address"></input>
-                <button className="flex-1 text-white bg-red-600 p-2">Sign Up</button>
-              </form>
-            </div>
-          </div>
-
-          <div className="hidden md:flex justify-between">
-            <Image className="rounded-xl mx-3" alt="chess piece" width={300} height={250} src="https://images.unsplash.com/photo-1538221566857-f20f826391c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80"/>
-          </div>
-        </div>
-        
-        <div className="items-center text-center mt-6">
-          <div>Integrated with:</div>
-          <div className="font-semibold">
-            lichess &#8226; chess.com
-          </div>
-        </div>
-
-        <div className="flex align-center justify-center flex-wrap mt-6">
-          <div style={cardStyle} className="hover:!text-blue-400 hover:!border-blue-400 md:max-w-xs">
-            <div className="flex items-center">
-              <h2 className="!mr-4">Hands Free</h2>
-              <div className="block">
-                <Image alt="clock" height={30} width={30} src={"/sync.png"}/>
+                <h5 className="text-lg md:text-xl mt-2">
+                  Sell your mobile home for cash!
+                </h5>
+              </div>
+              <div className="hidden sm:block">
+                <InfoForm setSuccessToast={setSuccessToast} setFailToast={setFailToast}/>
               </div>
             </div>
-            <p>Your games automatically sync with Async Coach, no need to upload anything</p>
+          </div>
+        </div>
+
+        <div className="items-center text-center mt-6 mx-4">
+          <div className="text-xl">How it works:</div>
+          <div className="font-semibold">
+            From the first conversation to sale we can close in under 7 days
+          </div>
+        </div>
+
+        <div className="flex align-center justify-center flex-wrap mt-2 mx-4">
+          <div style={cardStyle} className="hover:!text-blue-400 hover:!border-blue-400 md:max-w-xs">
+            <div className="flex items-center">
+              {/* <h2 className="!mr-4">Hands Free</h2> */}
+              <div className="block mx-auto mb-2">
+                <Image alt="clock" height={40} width={40} src={"/sync.png"}/>
+              </div>
+            </div>
+            <p>An initial call will help us understand your home and situation.</p>
           </div>
 
-          <div style={cardStyle} className="hover:!text-blue-400 hover:!border-blue-400 md:max-w-xs md:mx-4">
+          <div style={cardStyle} className="hover:!text-blue-400 hover:!border-blue-400 md:max-w-xs md:mx-4 my-2 md:my-0">
             <div className="flex items-center">
-              <h2 className="!mr-4">Personalized</h2>
-                <div className="block">
+              {/* <h2 className="!mr-4">Personalized</h2> */}
+                <div className="block mx-auto mb-2">
                   <Image alt="personalized" height={40} width={40} src={"/exchange.png"}/>
                 </div>
             </div>
-            <p>Let masters look at your play and pin point exactly what you need to play better chess</p>
+            <p>Schedule a walkthrough or send us pictures from various locations.</p>
           </div>
 
           <div style={cardStyle} className="hover:!text-blue-400 hover:!border-blue-400 md:max-w-xs">
             <div className="flex items-center">
-              <h2 className="!mr-4">Save Time</h2>
-              <div className="block">
+              {/* <h2 className="!mr-4">Save Time</h2> */}
+              <div className="block mx-auto mb-2">
                 <Image alt="clock" height={40} width={40} src={"/clock.png"}/>
               </div>
             </div>
-            <p>Request individual games to be reviewed and get feedback by the next day</p>
+            <p>Receive your cash and we will cover any transportation.</p>
           </div>
         </div>
 
-        <div className="justify-center mt-10 items-center md:flex mx-auto">
-          <Image alt="Chess Review" width={400} height={400} src={"/chessBoard.png"}/>
-          <div className="md:w-1/3 ml-2 p-2 bg-slate-200 rounded-xl">
-            <div className="mb-2">Against these stonewall structures make sure to lock down the center and finish developing before attacking on the flank.</div>
-            <div className="flex items-center">
-              <Image className="rounded-full" alt="master" width={30} height={30} src={"/chad.jpeg"}/>
-              <p className="ml-1">FM Chadley Gigaton (2469)</p>
+        <div className="w-8/12 mx-auto my-10">
+          <h2 className="mb-1 font-bold text-xl">Mobile Home Buyer in Augusta, Georgia</h2>
+          Located in <b>Augusta, GA</b> and need to sell your mobile or manufactured home? We are mobile home buyers and we make cash offers on <b>as-is</b> mobile and manufactured homes in Augusta, GA.
+        </div>
+
+        <div className="w-8/12 mx-auto my-10">
+          <h2 className="mb-1 font-bold text-xl">Do we purchase the land or just the home?</h2>
+          We can purchase any of the land and/or home. If you&apos;re looking to move quickly and don&apos;t have the time or the energy to remove debris from the land then we&apos;ll gladly handle that for you.
+        </div>
+
+        <div className="w-8/12 mx-auto my-10">
+          <h2 className=" mb-1 font-bold text-xl">Are you in any of these situations?</h2>
+          <ul className=" list-disc">
+            <li>Need to sell as-is, for cash?</li>
+            <li>Selling your home without land?</li>
+            <li>In a position where you need to sell FAST?</li>
+            <li>Tenants are always late with rent and you&apos;ve had enough?</li>
+            <li>Don&apos;t want to pay agent commision on the sale?</li>
+          </ul>
+          <h2 className=" mb-1 text-lg mt-3 font-semibold">If so please give us a call at 916-912-8488 and let&apos;s work together on a solution.</h2>
+        </div>
+
+        <div className=" max-w-2xl justify-center mx-auto text-center my-3">
+          <p className="text-lg mb-1 font-semibold">Who are we?</p>
+          <div className="justify-center items-center md:flex mx-auto">
+            <div className="flex-1">
+            <Image alt="Chess Review" width={300} height={300} src={"/jesusAndJordan.jpeg"}/>
+            </div>
+            <div className="md:w-1/3 mx-4 md:mx-2 p-2 bg-slate-200 rounded-xl flex-1">
+              <div className="">Hi, i&apos;m Jesus and this is my fiance Jordan.<br/><br/>We&apos;re passionate about housing and enjoy helping others with their housing needs.<br/><br/> I do software engineering and Jordan runs an environmental consulting company.</div>
             </div>
           </div>
         </div>
 
-        <div className="bg-opacity-50 mt-10 text-lg md:text-xl justify-center text-center">
-          <p>Sign up to be notified of the beta release</p>
-          <form className=" bg-opacity-50 bg-black inline-flex" onSubmit={(e) => submitEmail(e, email)}>
-            <input name="email" value={email} onChange={(e) => setEmail(e.target.value)} className="flex-4 border border-gray-500 pl-1" placeholder="email address"></input>
-            <button className="flex-1 text-white bg-red-600 p-2">Sign Up</button>
-          </form>
+        <div className=" mx-auto md:w-6/12 bg-opacity-50 mt-10 text-lg md:text-xl justify-center text-center">
+          <InfoForm setSuccessToast={setSuccessToast} setFailToast={setFailToast}/>
         </div>
       </main>
-      <footer className="flex flex-1 py-4 mt-8 border-t justify-center items-center">
+      <footer className="flex flex-1 py-4 mt-8 border-t justify-center items-center mx-2">
         <div className="block">
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className=" h-4 ml-2">
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-        <div className="block">
-          <div>
-            <a href="https://www.flaticon.com/free-icons/quick" title="quick icons">Quick icons created by kornkun - Flaticon</a>
+          <a
+            className="mx-auto"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <span className=" h-4 ml-2">
+              <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            </span>
+          </a>
+          <div className="grid grid-cols-4">
+            <div>
+              <a href="https://www.flaticon.com/free-icons/quick" title="quick icons">Quick icons created by kornkun - Flaticon</a>
+            </div>
+            <div>
+              <a href="https://www.flaticon.com/free-icons/personalization" title="personalization icons">Personalization icons created by Freepik - Flaticon</a>
+            </div>
+            <div>
+              <a href="https://www.flaticon.com/free-icons/sync" title="sync icons">Sync icons created by Freepik - Flaticon</a>
+            </div>
+            <div>
+            <a href="https://iconscout.com/icons/chess-strategy">Chess strategy Icon</a> by <a href="https://iconscout.com/contributors/iconscout">Iconscout Store</a>
+            </div>
           </div>
-          <div>
-            <a href="https://www.flaticon.com/free-icons/personalization" title="personalization icons">Personalization icons created by Freepik - Flaticon</a>
-          </div>
-          <div>
-            <a href="https://www.flaticon.com/free-icons/sync" title="sync icons">Sync icons created by Freepik - Flaticon</a>
-          </div>
-          <div>
-          <a href="https://iconscout.com/icons/chess-strategy">Chess strategy Icon</a> by <a href="https://iconscout.com/contributors/iconscout">Iconscout Store</a>
-          </div>
-        </div>
         </div>
       </footer>
 
       <Toasts successToast={successToast} setSuccessToast={setSuccessToast} failToast={failToast} setFailToast={setFailToast}/>
-
-      {showSlider && <ReadySlider setShowSlider={setShowSlider}/>}
     </div>
   )
 }
